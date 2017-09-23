b4b84b1626a39237410ca07555df87e15db1387c
wwwhatley <30512540+wwwhatley@users.noreply.github.com>
2017-09-22 21:24:04 -0400

    Worked on componenets.

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,22 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+
+  addFish(fish) {
+    //update our state
+    const fishes = {....this.state.fishes};
+    //add in our new fishes
+    const timestamp = Date.now();
+    fishes['fish-@[]']
+    //set state
+  }
   render() {
     return(
       <div className="catch-of-the-day">
